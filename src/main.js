5b03fee1606ab96b8c487d760149ad9cabdf303c
Rick Buitrago <rbuitragoc@gmail.com>
2017-10-27 10:19:47 -0500

    integrated web3 and metamask

--- src/main.js
+++ src/main.js
@@ -3,13 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Web3 from 'web3'
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+let web3 = {}
+window.addEventListener('load', () => {
+  if (typeof web3 !== 'undefined') {
+    console.log('Metamask Injected')
+    web3 = new Web3(web3.currentProvider)
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+    if (web3 !== 'undefined') {
+      console.log('Local RPC service loaded')
+    } else {
+      console.error('Provider not found')
+    }
+  }
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+  })
 })
